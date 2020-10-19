20e6c4d2a0524c3e1f9f84c7a6a0326e49a325ec
Isaac Tait <isaac@mountaintopcoding.com>
2020-10-18 20:09:19 -0700

    removing problem code all together

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,5 +1,5 @@
 <script>
-	import successkid from './images/successkid.jpg';
+
 </script>
 
 <style>
@@ -41,10 +41,5 @@
 </svelte:head>
 
 <h1>Great success!</h1>
-
-<figure>
-	<img alt="Success Kid" src="{successkid}">
-	<figcaption>Have fun with Sapper!</figcaption>
-</figure>
 
 <p><strong>Try editing this file (src/routes/index.svelte) to test live reloading.</strong></p>
